c522d71531e991b93bdd7e1e87c738c54321bcbf
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-09-25 23:38:08 +0200

    Feature: Creating the EmployeeDao class - work in progress.

--- src/mian/registration/dao/EmployeeDao.java
+++ src/mian/registration/dao/EmployeeDao.java
@@ -1,5 +1,17 @@
 package mian.registration.dao;
 
+import mian.registration.model.Employee;
+
 public class EmployeeDao {
 
+	public int registerEmployee(Employee employee) throws ClassNotFoundException {
+		String INSERT_USERS_SQL = "INSERT INTO employee"
+				+ " (id, first_name, last_name, username, password, address, contact) "
+				+ "VALUES (?,?,?,?,?,?,?);";
+		int result =0;
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		
+	}
 }
