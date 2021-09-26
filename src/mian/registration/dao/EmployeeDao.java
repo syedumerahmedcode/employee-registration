5e2b502dfb3997efa6bb0c9c60035fa25c93bc71
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-09-26 08:06:58 +0200

    Feature: Create the structure of EmployeeDao class.

--- src/mian/registration/dao/EmployeeDao.java
+++ src/mian/registration/dao/EmployeeDao.java
@@ -2,6 +2,8 @@ package mian.registration.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import mian.registration.model.Employee;
 
@@ -19,9 +21,27 @@ public class EmployeeDao {
 		int result =0;
 		
 		Class.forName("com.mysql.jdbc.Driver");
-		try {
+		try (
 			Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSL=false", "root", "root");
+			
+			// Step 2: Create a statement using connection object
+			PreparedStatement preparedStatement=connection.prepareStatement(INSERT_USERS_SQL)){
+			preparedStatement.setInt(1, 1);
+			preparedStatement.setString(2, employee.getFirstName());
+			preparedStatement.setString(3, employee.getLastName());
+			preparedStatement.setString(4, employee.getUsername());
+			preparedStatement.setString(5, employee.getPassword());
+			preparedStatement.setString(6, employee.getAddress());
+			preparedStatement.setString(7, employee.getContact());
+			
+			System.out.println("The prepared statement is: "+preparedStatement);
+			
+			// Step 3: Execute the query
+			result= preparedStatement.executeUpdate();
+			
+		}catch (SQLException ex) {
+			ex.printStackTrace();
 		}
-		
+		return result;
 	}
 }
